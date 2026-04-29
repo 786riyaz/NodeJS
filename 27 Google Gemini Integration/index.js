26f2aee8b5d1abea42931b185b35ae5fb25156ba
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-04-29 13:20:25 +0530

    Added Code for Google Gemini Integration

---
+++ 27 Google Gemini Integration/index.js
@@ -0,0 +1,35 @@
+/*
+// Setup
+# Initialize project
+npm init -y
+
+# Install the official Google Gen AI SDK
+npm install @google/genai
+
+npm install dotenv
+
+"type": "module"
+*/
+
+import { GoogleGenAI } from "@google/genai";
+import 'dotenv/config';
+
+// Initialize the client
+const ai = new GoogleGenAI({ 
+    apiKey: process.env.GEMINI_API_KEY 
+});
+
+async function main() {
+    try {
+        const response = await ai.models.generateContent({
+            model: "gemini-3-flash-preview",
+            contents: "Explain the benefits of Node.js in one sentence.",
+        });
+
+        console.log(response.text);
+    } catch (error) {
+        console.error("Error generating content:", error);
+    }
+}
+
+main();
